50838a5a01e96222c91bf832c3252e71cad41ebe
salzanopm <salzanopm@gmail.com>
2022-01-26 17:15:53 +0100

    first commit

---
+++ index-php.php
@@ -0,0 +1,36 @@
+<?php
+require '\database.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Google Faq</title>
+</head>
+<body>
+    <header>
+        <img src="img/spotify.png" alt="logo spotify">
+    </header>
+    <section>
+        <div >
+            <?php foreach($database as $single_song) { ?>
+                <h2><?php echo $single_song['title']; ?></h2>
+                <p><?php echo $single_song['author']; ?></p>
+            <?php } ?>
+        </div>
+    </section>
+
+    <footer>
+
+    </footer>
+
+</body>
+</html>
+
+
+
